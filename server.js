1194b61e2d84ac9ccb2d9211149650a010ddd44b
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-05-03 17:32:32 +0300

    feat(api): add Butter framework and implement posts API with user mapping

--- server.js
+++ server.js
@@ -1,1 +1,51 @@
-const Butter = require("../butter");
+const Butter = require("./butter");
+
+const USERS = [
+  { id: 1, name: "Liam Brown", username: "liam23", password: "string" },
+  { id: 2, name: "Meredith Green", username: "merit.sky", password: "string" },
+  { id: 3, name: "Ben Adams", username: "ben.poet", password: "string" },
+];
+
+const POSTS = [
+  {
+    id: 1,
+    title: "This is a post title",
+    body: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    userId: 1,
+  },
+];
+
+const PORT = 8000;
+
+const server = new Butter();
+
+// ------ Files Routes ------ //
+
+server.route("get", "/", (req, res) => {
+  res.sendFile("./public/index.html", "text/html");
+});
+
+server.route("get", "/styles.css", (req, res) => {
+  res.sendFile("./public/styles.css", "text/css");
+});
+
+server.route("get", "/scripts.js", (req, res) => {
+  res.sendFile("./public/scripts.js", "text/javascript");
+});
+
+// ------ JSON Routes ------ //
+
+// Send the list of all the posts that we have
+server.route("get", "/api/posts", (req, res) => {
+  const posts = POSTS.map((post) => {
+    const user = USERS.find((user) => user.id === post.userId);
+    post.author = user.name;
+    return post;
+  });
+
+  res.status(200).json(posts);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
+});
